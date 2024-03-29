42b74428bf5c0cd6374bfc089de242395bf36b29
gomitobasu <kunonodesign@gmail.com>
2024-03-29 10:41:08 +0900

    addAnimation

--- main.js
+++ main.js
@@ -9,3 +9,44 @@ menu.addEventListener("click", () => {
     menuButton.classList.toggle("on");
     menu.classList.toggle("on");
   });
+
+  window.onload = function () {
+    //lax.jsを初期化
+    lax.init()
+  
+    // ドライバーを追加する
+    lax.addDriver('scrollY', function () {
+      return window.scrollY;
+    });
+  
+    // 要素にアニメーションを紐づける
+    lax.addElements('.cardsOne', {
+      scrollY: {
+        translateX: [
+        ['elInY', 'elHeight/1.2'],
+        [-300, 0]
+        ],
+        opacity:[
+        ['elInY', 'elHeight/1.2'],
+        [0, 1]
+        ],
+        rotate:[
+            ['elInY', 'elHeight/1.2'],
+            [-15, 0]
+            ]
+      }
+    });
+    lax.addElements('.cardsTwo', {
+        scrollY: {
+          opacity: [
+          ['elInY', 'elCenterY-elHeight/1.5'],
+          [0, 1]
+          ],
+          rotate:[
+            ['elInY', 'elHeight/2'],
+            [-15, 0]
+            ]
+        }
+      });
+  }
+  
